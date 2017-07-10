8b36dfc6e1fab2792692413ccbefb7aeb5cc5f53
Luis Manuel Rodriguez <lwe_@hotmail.com>
2017-07-10 15:36:28 -0500

    added find controller

--- www/app/app.routes.js
+++ www/app/app.routes.js
@@ -10,54 +10,15 @@
  function initializeRoutes($stateProvider, $urlRouterProvider){
 
    $stateProvider
-
-    .state('tab', {
-     url: '/tab',
-     abstract: true,
-     templateUrl: 'templates/tabs.html'
+    .state('find', {
+     url: '/find',
+     templateUrl: 'app/songs/find.html',
+     controller:"FindController",
+     controllerAs: "vm"
    })
-
-   .state('tab.dash', {
-     url: '/dash',
-     views: {
-       'tab-dash': {
-         templateUrl: 'templates/tab-dash.html',
-
-       }
-     }
-   })
-
-   .state('tab.chats', {
-       url: '/chats',
-       views: {
-         'tab-chats': {
-           templateUrl: 'templates/tab-chats.html',
-
-         }
-       }
-     })
-     .state('tab.chat-detail', {
-       url: '/chats/:chatId',
-       views: {
-         'tab-chats': {
-           templateUrl: 'templates/chat-detail.html',
-
-         }
-       }
-     })
-
-   .state('tab.account', {
-     url: '/account',
-     views: {
-       'tab-account': {
-         templateUrl: 'templates/tab-account.html',
-
-       }
-     }
-   });
 
    // if none of the above states are matched, use this as the fallback
-   $urlRouterProvider.otherwise('/tab/dash');
+   $urlRouterProvider.otherwise('/find');
 
  }
 
